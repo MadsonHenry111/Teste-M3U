c8e5b86a9be495682ae0fbe4346f0e0983f4a148
MadsonHenry111 <madsonhenry12@outlook.com>
2025-11-24 20:31:27 -0300

    Add files via upload

---
+++ Epg.js
@@ -0,0 +1,9 @@
+function carregarEPG() {
+    document.getElementById("epg").innerHTML = `
+        <h3>Guia de Programação</h3>
+        <p>08:00 - Programa 1</p>
+        <p>10:00 - Notícias</p>
+        <p>12:00 - Filme</p>
+        <p>14:00 - Série</p>
+    `;
+}
