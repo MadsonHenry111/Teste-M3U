fcccce42c2a7483ba02969db9cdd4b362227bf5d
MadsonHenry111 <madsonhenry12@outlook.com>
2025-11-24 20:07:07 -0300

    Add files via upload

---
+++ Epg.js
@@ -0,0 +1,10 @@
+{
+    "Canal Exemplo 1": [
+        { "hora": "10:00", "titulo": "Programa 1" },
+        { "hora": "11:00", "titulo": "Filme Ação" }
+    ],
+    "Canal Exemplo 2": [
+        { "hora": "09:30", "titulo": "Notícias" },
+        { "hora": "10:00", "titulo": "Esportes ao Vivo" }
+    ]
+}
