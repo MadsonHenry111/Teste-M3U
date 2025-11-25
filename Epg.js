d1c33c32e8e2eb551e1cfcf7a4604cf4fd4441ad
MadsonHenry111 <madsonhenry12@outlook.com>
2025-11-25 09:10:59 -0300

    Delete Epg.js

--- Epg.js
+++
@@ -1,9 +0,0 @@
-function carregarEPG() {
-    document.getElementById("epg").innerHTML = `
-        <h3>Guia de Programação</h3>
-        <p>08:00 - Programa 1</p>
-        <p>10:00 - Notícias</p>
-        <p>12:00 - Filme</p>
-        <p>14:00 - Série</p>
-    `;
-}
